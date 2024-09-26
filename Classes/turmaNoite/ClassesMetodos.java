221935e0235ffa0c92d1c02448ecee64b3c574a4
Rubens Rocha <rub.s2rocha@gmail.com>
2024-09-25 21:44:46 -0300

    Metodos soma, parimpar, stringinvertida, fatorial

--- Classes/turmaNoite/ClassesMetodos.java
+++ Classes/turmaNoite/ClassesMetodos.java
@@ -1,9 +1,50 @@
 package turmaNoite;
 
 public class ClassesMetodos {
-
+	
+	//01 Calcula a soma de 2 valores
+	public int soma (int a, int b) {
+		return a + b;
+	}
+	//02 par impar
+	public boolean parImpar(int a) {
+	
+			return a % 2 == 0;
+	}
+	//03 inverter uma string
+	public String invertString(String nome) {
+		return new StringBuilder(nome).reverse().toString();
+	}
+	
+	//04 Metodo  Fatorial
+	
+	public int fatorial(int numero) {
+ 
+		if (numero ==0 || numero ==1) {
+			return 1;
+		}
+		return numero * fatorial(numero -1);
+	}
+	
+	//05 Encontra o maior numero em um array de inteiros
+	
+	
+	
+	
+	
+    //Este método executa outros metodos!
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Instanciando a classe que criamos:
+		ClassesMetodos util = new ClassesMetodos();
+		
+		//Chamando os métodos:
+		System.out.println("Soma de 5 é 3: " + util.soma(5, 3));
+		
+		System.out.println("O numero 4 é par?  " + util.parImpar(4));
+	    
+		System.out.println("Nome ao contrario: " + util.invertString("java"));
+		
+		System.out.println("O fatorial do numero 5 é " + util.fatorial(5));
 
 	}
 
