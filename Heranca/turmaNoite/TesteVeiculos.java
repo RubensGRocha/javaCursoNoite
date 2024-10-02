7936d1495dd470f9896a5073aa321a192126dbe4
Rubens Rocha <rub.s2rocha@gmail.com>
2024-10-01 21:36:39 -0300

    Herança01

--- Heranca/turmaNoite/TesteVeiculos.java
+++ Heranca/turmaNoite/TesteVeiculos.java
@@ -3,14 +3,18 @@ package turmaNoite;
 public class TesteVeiculos {
 	public static void main(String[] args) {
 		Carro carro = new Carro("Toyota", "Corola", 2004, 4);
+		Carro carro01 = new Carro("Fiat", "Toro", 2020, 4);
 		Moto moto = new Moto("Honda ", "CB500",2019 , true);
+		Moto moto01 = new Moto("Yamaha ", "Nmax",2020 , true);
 		
 		
 		System.out.println("Detalhes do Carro: ");
 		carro.exibirDetalhes();
+		carro01.exibirDetalhes();
 		
-		System.out.println("Detalhes da moto: ");
+		System.out.println("\nDetalhes da moto: ");
 		moto.exibirDetalhes();
+		moto01.exibirDetalhes();
 
 	}
 
