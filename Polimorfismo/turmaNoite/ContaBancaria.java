230fe9bef890d3c5b39f40520891eb31ece14308
Rubens Rocha <rub.s2rocha@gmail.com>
2024-10-02 19:50:05 -0300

    Polimorfismo

--- Polimorfismo/turmaNoite/ContaBancaria.java
+++ Polimorfismo/turmaNoite/ContaBancaria.java
@@ -2,26 +2,36 @@ package turmaNoite;
 
 public class ContaBancaria {
 
-	public static void main(String[] args) {
-		// Criando atributo privado
-		private double saldo;
-		
-		//Criando método contrutor para iniciar o saldo:
-		public ContaBancaria(double saldoInicial) {
-			this.saldo = saldoInicial;
-		}
-		//público para depositar dinheiro na conta:
-		public void depositar(double valor) {
-			if (valor > 0) {
-				saldo += valor; //saldo +saldo + valor
-				System.out.println("Deposito de R$ " +valor+" realizado!");
-			} else {
-				System.out.println("Valor depositado invalido");
-				}
-			
-		     
-			
-	
+	// Criando atributo privado
+	private double saldo;
 
-}
+	// Criando método contrutor para iniciar o saldo:
+	public ContaBancaria(double saldoInicial) {
+		this.saldo = saldoInicial;
+	}
+
+	// Metodo público para depositar dinheiro na conta:
+	public void depositar(double valor) {
+		if (valor > 0) {
+			saldo += valor; // saldo +saldo + valor
+			System.out.println("Deposito de R$ " + valor + " realizado!");
+		} else {
+			System.out.println("Valor depositado invalido ");
+		}
+	}
+
+	public void sacar(double valor) {
+		if (valor > 0 && valor <= saldo) {
+			saldo -= valor;
+			System.out.println("Saque de R$ " + valor);
+		} else {
+			System.out.println("Seu saldo é insuficiente");
+		}
+
+	}
+
+	// Método público para consultar o saldo (getter):
+	public double getSaldo() {
+		return saldo;
+	}
 }
--- Polimorfismo/turmaNoite/OperaConta.java
+++ Polimorfismo/turmaNoite/OperaConta.java
@@ -3,8 +3,21 @@ package turmaNoite;
 public class OperaConta {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Criação de conta com saldo inicial de R$ 1000.00:
+		ContaBancaria conta = new ContaBancaria(1000.00);
+		
+		//Operando a conta do cliente:
+		//Exibir o saldo inicial da conta:
+		System.out.println("Saldo Inicial " + conta.getSaldo() );
+		
+		
+		//Realizando Depósito:
+		conta.depositar(5);
+		System.out.println("Seu saldo é  "+ conta.getSaldo());
 
+		//Realizar um saque:
+		conta.sacar(50);
+		System.out.println("Seu saldo é " + conta.getSaldo());
 	}
 
 }
